b71cf79c24be02542bae867431c3f435e7c17e82
陳煜翔 <chenyuxiang@chenyuxiangdeMacBook-Air.local>
2025-05-26 02:01:08 +0800

    fix vercel cors problem

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -47,7 +47,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://*.vercel.app")); // 前端網址
+        // configuration.setAllowedOrigins(List.of("http://localhost:5173","https://*.vercel.app")); // 前端網址
+        configuration.setAllowedOriginPatterns(List.of(
+            "http://localhost:5173",
+            "https://*.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
