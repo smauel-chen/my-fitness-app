1084fdc359299b65551be94a11fc2f8f9910f116
陳煜翔 <chenyuxiang@chenyuxiangdeMacBook-Air.local>
2025-05-26 00:17:24 +0800

    deploy ready

--- my-fitness-app/src/api/axiosInstance.js
+++ my-fitness-app/src/api/axiosInstance.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
+// const instance = axios.create({
+//   baseURL: "http://localhost:8080",
+// });
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // 每次請求都自動加上 token
 instance.interceptors.request.use(
   (config) => {
+    console.log("API base URL: ", import.meta.env.VITE_API_URL);
     const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
